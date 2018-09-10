2661f39c3ce08e246817bd0c0b4e644b442501af
a503-02 <a503-02@localhost>
2018-09-10 17:50:30 +0900

    Upload

---
+++ 0910class/src/dao/PdsDAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import vo.Pds;
+
+public interface PdsDAO {
+	//Pds 테이블에서 가장 큰 code(숫자)를 찾아오는 메소드
+	public int maxCode();
+	
+	//Pds 테이블에 데이터를 삽입하는 메소드
+	public boolean insertPds(Pds pds);
+	
+	
+
+}
